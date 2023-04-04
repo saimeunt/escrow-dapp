f9f05f2b2cb677fda3507b2b5b9536e9f987d2ea
Simon <saimeunt@gmail.com>
2023-04-05 00:34:09 +0200

    fix getlogs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,6 @@
 import { uniq } from 'lodash';
 
 import contract from './contract';
-import alchemy from './alchemy';
 
 export const truncateAddress = (address: string) =>
   `${address.substring(0, 8)}…${address.substring(address.length - 8)}`;
@@ -9,17 +8,13 @@ export const truncateAddress = (address: string) =>
 export const getLatestEscrows = () => contract.getLatestEscrows();
 
 export const getEscrowsByAddress = async (address: string) => {
-  const getLogs =
-    process.env.NODE_ENV !== 'production'
-      ? contract.provider.getLogs.bind(contract.provider)
-      : alchemy.core.getLogs;
   const depositorFilters = contract.filters.EscrowCreated(address);
   const arbiterFilters = contract.filters.EscrowCreated(null, address);
   const beneficiaryFilters = contract.filters.EscrowCreated(null, null, address);
   const [depositorLogs, arbiterLogs, beneficiaryLogs] = await Promise.all([
-    getLogs({ ...depositorFilters, fromBlock: 0 }),
-    getLogs({ ...arbiterFilters, fromBlock: 0 }),
-    getLogs({ ...beneficiaryFilters, fromBlock: 0 }),
+    contract.provider.getLogs({ ...depositorFilters, fromBlock: 0 }),
+    contract.provider.getLogs({ ...arbiterFilters, fromBlock: 0 }),
+    contract.provider.getLogs({ ...beneficiaryFilters, fromBlock: 0 }),
   ]);
   const logs = [...depositorLogs, ...arbiterLogs, ...beneficiaryLogs];
   const ids = uniq(logs.map(({ data }) => data));
