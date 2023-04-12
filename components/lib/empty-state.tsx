16f6ab3fd63274d5017f532e819180e7d73f7c90
Simon <saimeunt@gmail.com>
2023-04-12 21:32:06 +0200

    update

--- components/lib/empty-state.tsx
+++ components/lib/empty-state.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { PlusIcon } from '@heroicons/react/20/solid';
 import { BuildingLibraryIcon } from '@heroicons/react/24/outline';
+import { useIsClient } from 'usehooks-ts';
+import { useAccount } from 'wagmi';
+import { Web3Button } from '@web3modal/react';
 
 import useContext from './context/hook';
 
 const EmptyState = () => {
   const { openCreateEscrowModal } = useContext();
+  const { isConnected } = useAccount();
+  const isClient = useIsClient();
   return (
     <div className="text-center">
       <BuildingLibraryIcon className="mx-auto mt-4 h-12 w-12 text-gray-400" aria-hidden="true" />
@@ -14,14 +19,18 @@ const EmptyState = () => {
       </h3>
       <p className="mt-1 text-sm text-gray-500">How about creating one?</p>
       <div className="mt-6">
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          onClick={openCreateEscrowModal}
-        >
-          <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-          Create Escrow
-        </button>
+        {isClient && isConnected ? (
+          <button
+            type="button"
+            className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={openCreateEscrowModal}
+          >
+            <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
+            Create Escrow
+          </button>
+        ) : (
+          <Web3Button />
+        )}
       </div>
     </div>
   );
--- components/lib/web3modal.tsx
+++ components/lib/web3modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { EthereumClient } from '@web3modal/ethereum';
 import { Web3Modal } from '@web3modal/react';
+import { indigo } from 'tailwindcss/colors';
 
 import {
   developmentChains,
@@ -13,6 +14,7 @@ const CustomWeb3Modal = () => (
   <Web3Modal
     projectId={process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID}
     themeMode="light"
+    themeVariables={{ '--w3m-accent-color': indigo[600], '--w3m-background-color': indigo[600] }}
     ethereumClient={
       new EthereumClient(
         process.env.NODE_ENV !== 'production' ? developmentWagmiClient() : productionWagmiClient(),
